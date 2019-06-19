1700459d69f4bd39af9ca03f102a3a227fe809e5
Manvi Tyagi <manvityagi770@gmail.com>
2019-06-19 17:51:27 +0530

    cleaning

--- app/routes/client/index.js
+++ app/routes/client/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 router.use('/coupons',require('./coupon'));
 router.use('/events',require('./event'));
 router.use('/venues',require('./venue'));
+router.use('/payments',require('./payment'));
 router.use('/',require('./login'));
 
 module.exports = router;
--- app/routes/payment/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.use('/',require('./CRUD_payment'));
-router.use('/',require('./monthlyPaymnet'));
-router.use('/',require('./tillNow'));
-
-
-module.exports = router;
