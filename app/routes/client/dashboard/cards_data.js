3bd09f0bdd9664607587406c6b0ef015eced0ee2
Manvi Tyagi <manvityagi770@gmail.com>
2019-06-27 11:25:51 +0530

    card_data attributes added

---
+++ app/routes/client/dashboard/cards_data.js
@@ -0,0 +1,11 @@
+                                   //client/dashboard/:client_id/stats
+
+//1. total events done by a client  
+//2. total upcoming events
+//3. cancelled events
+//4. total business done
+//5. no of venues registered
+//6. total tickets sold
+//7. 
+//8. 
+
--- app/routes/client/index.js
+++ app/routes/client/index.js
@@ -4,6 +4,7 @@ router.use('/coupons',require('./coupon'));
 router.use('/events',require('./event'));
 router.use('/venues',require('./venue'));
 router.use('/payments',require('./payment'));
+router.use('/dashboard',require('./dashboard'));
 //router.use('/',require('./login'));
 
 module.exports = router;
